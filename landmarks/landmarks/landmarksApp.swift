f8c3869f53cd2f8701df7b1b69f12650209bf746
tomonakar <tomohisa.nak@gmail.com>
2022-04-23 17:14:00 +0900

    section1 initial commit

---
+++ landmarks/landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by tomonakar on 2022/04/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
